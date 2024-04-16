3e7c71d329a4fdc883387e710ef8c33bfa57a2c9
Patrick Kabwe <patrickckabwe@gmail.com>
2024-04-16 09:27:29 +0200

    chore: export all funcs

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import FacebookLoginButton from "./components/facebook-login-button";
+export * from "./components/facebook-provider";
+export * from "./hook/use-facebook-login";
+
+export default FacebookLoginButton;
