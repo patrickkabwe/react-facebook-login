db8ffd6b0871da5553f0289bd299409f8fe0fb45
Patrick Kabwe <patrickckabwe@gmail.com>
2025-01-11 11:30:05 +0200

    fix: test

--- src/hook/use-facebook-login.ts
+++ src/hook/use-facebook-login.ts
@@ -1,32 +1,32 @@
 import { useCallback } from "react";
-import { UseFacebookLoginOptions } from "../types/react-facebook-login";
+import { FacebookLoginOptions } from "../types/react-facebook-login";
 import { getUserProfile } from "../utils";
 
 export const useFacebookLogin = ({
-  onSuccess,
-  onError,
-  ...rest
-}: UseFacebookLoginOptions) => {
-  const cb = useCallback(
-    (opt?: fb.LoginOptions) => {
-      window.FB.login((response) => {
-        if (response.authResponse) {
-          if (rest.shouldFetchUserProfile) {
-            getUserProfile((userResponse: fb.UserResponse) => {
-              (onSuccess as (response: fb.UserResponse) => void)(userResponse);
-            });
-          } else {
-            (onSuccess as (response: fb.StatusResponse) => void)(response);
-          }
-        } else {
-          if (onError && response.status !== "connected") {
-            onError(response);
-          }
-        }
-      }, opt);
-    },
-    [onError, onSuccess, rest.shouldFetchUserProfile]
-  );
+    onSuccess,
+    onError,
+    ...rest
+}: FacebookLoginOptions) => {
+    const cb = useCallback(
+        (opt?: fb.LoginOptions) => {
+            window.FB.login((response) => {
+                if (response.status === "connected") {
+                    if (rest.shouldFetchUserProfile) {
+                        getUserProfile((userResponse) => {
+                            onSuccess({ ...response, ...userResponse });
+                        });
+                    } else {
+                        onSuccess(response);
+                    }
+                } else {
+                    if (onError) {
+                        onError(response);
+                    }
+                }
+            }, opt);
+        },
+        [onError, onSuccess, rest.shouldFetchUserProfile]
+    );
 
-  return cb;
+    return cb;
 };
--- tests/facebook-login-button.test.tsx
+++ tests/facebook-login-button.test.tsx
@@ -1,140 +1,160 @@
 import { act, fireEvent, render } from "@testing-library/react";
+import { beforeEach, describe, expect, it, jest } from "bun:test";
 import { JSDOM } from "jsdom";
 import React from "react";
-import { beforeEach, describe, expect, it, jest } from "bun:test";
 import FacebookLoginButton, { FacebookProvider } from "../src";
 
 const TestApp = () => {
-  return (
-    <FacebookProvider appId="1234" version="v19.0">
-      <FacebookLoginButton onSuccess={(res) => {}} />
-    </FacebookProvider>
-  );
+    return (
+        <FacebookProvider appId="1234" version="v19.0">
+            <FacebookLoginButton onSuccess={(res) => {}} />
+        </FacebookProvider>
+    );
 };
 
 const mockFB = {
-  init: jest.fn(),
-  login: jest.fn(),
-  api: jest.fn(),
+    init: jest.fn(),
+    login: jest.fn(),
+    api: jest.fn(),
 };
 
 describe("FacebookLoginButton", () => {
-  let window;
+    let window;
 
-  beforeEach(() => {
-    const dom = new JSDOM("<!DOCTYPE html><html><body></body></html>");
-    window = dom.window;
-    globalThis.document = window.document;
-    globalThis.window = window;
+    beforeEach(() => {
+        const dom = new JSDOM("<!DOCTYPE html><html><body></body></html>");
+        window = dom.window;
+        globalThis.document = window.document;
+        globalThis.window = window;
 
-    mockFB.init.mockClear();
-    mockFB.login.mockClear();
-    window.FB = mockFB;
-    globalThis.FB = mockFB as any;
-  });
-
-  it("should render children", () => {
-    const { container } = render(<TestApp />);
-    expect(container.firstChild).not.toBeNull();
-  });
-
-  it("should load Facebook SDK and initialize it with the correct configuration", async () => {
-    render(<TestApp />);
-
-    const script = document.querySelector<HTMLScriptElement>(
-      'script[src="https://connect.facebook.net/en_US/sdk.js"]'
-    );
-
-    expect(script).toBeTruthy();
-
-    // @ts-ignore
-    act(script?.onload);
-
-    expect(window.FB.init).toHaveBeenCalledWith({
-      appId: "1234",
-      version: "v19.0",
-      xfbml: true,
+        mockFB.init.mockClear();
+        mockFB.login.mockClear();
+        window.FB = mockFB;
+        globalThis.FB = mockFB as any;
     });
 
-    expect(window.FB.init).toHaveBeenCalledTimes(1);
-  });
-
-  it("should call FB.login", () => {
-    const { getByTestId } = render(<TestApp />);
-    const button = getByTestId("facebook-login-button");
-
-    fireEvent.click(button);
-    expect(FB.login).toHaveBeenCalled();
-  });
-
-  it("should call success callback", () => {
-    // @ts-ignore
-    FB.login.mockImplementation((cb) => {
-      cb({
-        authResponse: {
-          accessToken: "1234",
-        },
-      });
+    it("should render children", () => {
+        const { container } = render(<TestApp />);
+        expect(container.firstChild).not.toBeNull();
     });
 
-    const onSuccess = jest.fn();
+    it("should load Facebook SDK and initialize it with the correct configuration", async () => {
+        render(<TestApp />);
 
-    const { getByTestId } = render(
-      <FacebookProvider appId="1234" version="v19.0">
-        <FacebookLoginButton onSuccess={onSuccess} />
-      </FacebookProvider>
-    );
-    const button = getByTestId("facebook-login-button");
-
-    fireEvent.click(button);
-
-    expect(onSuccess).toHaveBeenCalled();
-  });
-
-  it("should call success callback with user profile", () => {
-    // @ts-ignore
-    FB.api.mockImplementation((path, fields, cb) => {
-      if (path === "/me") {
-        cb(
-          fields.fields.reduce((acc, field) => {
-            acc[field] = field;
-            return acc;
-          }, {})
+        const script = document.querySelector<HTMLScriptElement>(
+            'script[src="https://connect.facebook.net/en_US/sdk.js"]'
         );
-      }
+
+        expect(script).toBeTruthy();
+
+        // @ts-ignore
+        act(script?.onload);
+
+        expect(window.FB.init).toHaveBeenCalledWith({
+            appId: "1234",
+            version: "v19.0",
+            xfbml: true,
+        });
+
+        expect(window.FB.init).toHaveBeenCalledTimes(1);
     });
 
-    const onSuccess = jest.fn();
+    it("should call FB.login", () => {
+        const { getByText } = render(<TestApp />);
+        const button = getByText("Login with Facebook");
 
-    const { getByTestId } = render(
-      <FacebookProvider appId="1234" version="v19.0">
-        <FacebookLoginButton onSuccess={onSuccess} shouldFetchUserProfile={true} />
-      </FacebookProvider>
-    );
-    const button = getByTestId("facebook-login-button");
-
-    fireEvent.click(button);
-
-    expect(onSuccess).toHaveBeenCalledWith({
-      name: "name",
-      email: "email",
-      picture: "picture",
+        fireEvent.click(button);
+        expect(FB.login).toHaveBeenCalled();
     });
-  });
 
-  it("should render custom component", () => {
-    const CustomComponent = () => (
-      <button data-testid="custom-button">Custom Button</button>
-    );
+    it("should call success callback", () => {
+        // @ts-ignore
+        FB.login.mockImplementation((cb) => {
+            cb({
+                authResponse: {
+                    accessToken: "1234",
+                },
+                status: "connected",
+            });
+        });
 
-    const { getByTestId } = render(
-      <FacebookProvider appId="1234" version="v19.0">
-        <FacebookLoginButton onSuccess={() => {}} component={CustomComponent} />
-      </FacebookProvider>
-    );
+        const onSuccess = jest.fn();
 
-    const button = getByTestId("custom-button");
-    expect(button).toBeTruthy();
-    expect(button.textContent).toBe("Custom Button");
-  });
+        const { getByText } = render(
+            <FacebookProvider appId="1234" version="v19.0">
+                <FacebookLoginButton onSuccess={onSuccess} />
+            </FacebookProvider>
+        );
+        const button = getByText("Login with Facebook");
+
+        fireEvent.click(button);
+
+        expect(onSuccess).toHaveBeenCalled();
+    });
+
+    it("should call success callback with user profile", () => {
+        // @ts-ignore
+        FB.login.mockImplementation((cb) => {
+            cb({
+                authResponse: {
+                    accessToken: "1234",
+                },
+                status: "connected",
+            });
+        });
+        // @ts-ignore
+        FB.api.mockImplementation((path, fields, cb) => {
+            if (path === "/me") {
+                cb(
+                    fields.fields.reduce((acc, field) => {
+                        acc[field] = field;
+                        return acc;
+                    }, {})
+                );
+            }
+        });
+
+        const onSuccess = jest.fn();
+
+        const { getByText } = render(
+            <FacebookProvider appId="1234" version="v19.0">
+                <FacebookLoginButton
+                    onSuccess={onSuccess}
+                    shouldFetchUserProfile={true}
+                />
+            </FacebookProvider>
+        );
+        const button = getByText("Login with Facebook");
+
+        fireEvent.click(button);
+
+        expect(onSuccess).toHaveBeenCalledWith({
+            authResponse: {
+                accessToken: "1234",
+            },
+            status: "connected",
+            name: "name",
+            email: "email",
+            picture: "picture",
+        });
+    });
+
+    it("should render custom component", () => {
+        const CustomComponent = () => (
+            <button data-testid="custom-button">Custom Button</button>
+        );
+
+        const { getByTestId } = render(
+            <FacebookProvider appId="1234" version="v19.0">
+                <FacebookLoginButton
+                    onSuccess={() => {}}
+                    component={CustomComponent}
+                />
+            </FacebookProvider>
+        );
+
+        const button = getByTestId("custom-button");
+        expect(button).toBeTruthy();
+        expect(button.textContent).toBe("Custom Button");
+    });
 });
