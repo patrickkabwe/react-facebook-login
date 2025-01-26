75b0ec7dbea3b5b937fc92b7fec9653218ee9b2e
Patrick Kabwe <patrickckabwe@gmail.com>
2025-01-26 06:49:07 +0200

    feat: facebook button accepts children

--- src/components/facebook-login-button.tsx
+++ src/components/facebook-login-button.tsx
@@ -1,30 +1,39 @@
-import { forwardRef, useCallback } from "react";
+import React, { forwardRef, PropsWithChildren, useCallback } from "react";
 import { useFacebookLogin } from "../hook/use-facebook-login";
 import { FacebookLoginProps } from "../types/react-facebook-login";
 
-const FacebookLoginButton = forwardRef<HTMLButtonElement, FacebookLoginProps>(
-    ({ onSuccess, onError, shouldFetchUserProfile, ...props }, ref) => {
-        const login = useFacebookLogin({
-            onSuccess,
-            onError,
-            shouldFetchUserProfile,
-        });
+const FacebookLoginButton = forwardRef<
+    HTMLButtonElement,
+    PropsWithChildren<FacebookLoginProps>
+>(({ onSuccess, onError, shouldFetchUserProfile, ...props }, ref) => {
+    const login = useFacebookLogin({
+        onSuccess,
+        onError,
+        shouldFetchUserProfile,
+    });
 
-        const handleLogin = useCallback(() => login(), [login]);
+    const handleLogin = useCallback(() => login(), [login]);
 
-        return props.component ? (
-            <props.component {...props} onClick={handleLogin} />
-        ) : (
-            <button
-                ref={ref}
-                {...props}
-                onClick={handleLogin}
-                data-testid="facebook-login-button"
-            >
-                Login with Facebook
-            </button>
-        );
-    }
-);
+    return props.children && React.Children.count(props.children) > 0 ? (
+        React.createElement(
+            "button",
+            {
+                ...props,
+                onClick: handleLogin,
+                ref,
+            },
+            props.children
+        )
+    ) : (
+        <button
+            ref={ref}
+            {...props}
+            onClick={handleLogin}
+            data-testid="facebook-login-button"
+        >
+            Login with Facebook
+        </button>
+    );
+});
 
 export default FacebookLoginButton;
